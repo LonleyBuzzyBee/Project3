b2b9d0c26a249ee0382df7cb1cc3e4aac8ac1d42
mai <mailiecook@yahoo.com>
2020-01-24 10:29:47 -0800

    add changes to scripts.js, working on funtionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 
+$(document).ready(function() {
+  $("form.form").submit(function(event) {
+    event.preventDefault();
+    var strInput = $("input.inputNumber").val();
+    var numberArray = strInput.split("");
+    for (var index = 0; index< numberArray.length; index++) {
+      total = numberArray[index];
+}
+  alert(total);
+  });
+});
